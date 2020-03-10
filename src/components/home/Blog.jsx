b8dd621d01ca60b8429811e09e5284c0570964f9
Andre Santa Cruz <asantacruz@cognitiva.com.pe>
2020-03-09 22:50:18 -0500

    update

--- src/components/home/Blog.jsx
+++ src/components/home/Blog.jsx
@@ -4,14 +4,14 @@ import Card from 'components/general/card/Card'
 import Text from 'components/general/text/Text'
 import MainContent from 'components/home/main-content/MainContent'
 import data from 'data/blog.json'
-import Filter from 'components/general/filter/Filter'
+import CardSlider from 'components/general/card-slider/CardSlider'
 
 const Blog = () => {
 
     return (
         <MainContent>
             <div className={styles.wrapper__text}>
-                <Text content="Neque porro quisquam est qui dolorem ipsum" className={styles.heading_santa_blog} headingLevel="h1"/>
+                <Text content="Neque porro quisquam est qui dolorem ipsum" className={styles.heading_santa_blog} headingLevel="h1" />
             </div>
             <div className={styles.wrapper__content}>
                 <div className={styles.inner_wrapper__content}>
@@ -25,6 +25,22 @@ const Blog = () => {
                         />
                     )}
                 </div>
+                <CardSlider
+                    autoplay
+                    delayLength={6000}
+                >
+                    <div className={styles.inner_wrapper__content}>
+                        {data.map(post =>
+                            <Card
+                                key={post.id}
+                                image={post.imagen}
+                                alt={post.alt}
+                                title={post.titulo}
+                                url={post.url}
+                            />
+                        )}
+                    </div>
+                </CardSlider>
             </div>
         </MainContent>
     )
